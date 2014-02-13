cf25c4162745453f506fecd62801649284edab49
ChasWoodfield <chas.woodfield@k-int.co.uk>
2014-02-13 09:37:17 +0000

    Committed a change by accident

--- test/functional/com/k_int/cultureGrid/Pages/Home/LoggedInHome.groovy
+++ test/functional/com/k_int/cultureGrid/Pages/Home/LoggedInHome.groovy
@@ -5,7 +5,7 @@ import geb.Page
 import com.k_int.cultureGrid.Modules.MenuOptions
 
 class LoggedInHome extends Page {
-	static url = "?__userid__=cgadmin&__password__=cgadmin"
+	static url = "?__userid__=XXX&__password__=YYY"
 	static at = {(title.endsWith("Home Page")) || (title.endsWith("Registered User Home"))};
 	
 	static content = {
