74583129252d28fc4db00902cf535256bc9d9fe2
ChasWoodfield <chas.woodfield@k-int.co.uk>
2014-02-24 10:22:10 +0000

    Added comments to the closures, and changed a couple of argument names so that they are more meaningful

--- test/functional/com/k_int/cultureGrid/Generic/BasePage.groovy
+++ test/functional/com/k_int/cultureGrid/Generic/BasePage.groovy
@@ -4,6 +4,7 @@ import geb.Page
 
 import com.k_int.cultureGrid.Modules.MenuOptions
 
+/** Base Page for all pages, this contains closures in the content that can be used by any of the pages */
 class BasePage extends Page {
 	static def ELEMENT_ANCHOR = "a"
 	static def ELEMENT_FORM   = "form"
@@ -13,17 +14,25 @@ class BasePage extends Page {
 	static def TYPE_SELECT   = "select"
 
 	static content = {
-		// Content that can be used by all the pages
+		/** Gives access to the menu options */
 		menu {module MenuOptions}
+		
+		/** Retrieves a field within a form given its name */
 		getField {type, fieldName ->
 				$(ELEMENT_FORM).find(type, name: fieldName)
 		}
+		
+		/** Retrieves an input field within a form given ite name */
 		getInputField {fieldName ->
 			getField(TYPE_INPUT, fieldName)
 		}
+		
+		/** Retrieves a select field within a form given ite name */
 		getSelectField {fieldName ->
 			getField(TYPE_SELECT, fieldName)
 		}
+
+		/** Retrieves and sets a fields value on a form given its name, if the value is null then the field is not set */		
 		getSetType {type, fieldName, value -> 
 			def field = getField(type, fieldName)
 			if (field.size() == 1) {
@@ -37,25 +46,42 @@ class BasePage extends Page {
 				// println "Not found field: \"" + fieldName + "\", type: \"" + type + "\""					
 			}
 		}
+
+		/** Retrieves and sets an input field value on a form given its name, if the value is null then the field is not set */		
 		getSetInput {fieldName, value ->
 			getSetType(TYPE_INPUT, fieldName, value)
 		} 
+
+		/** Retrieves and sets a select field value on a form given its name, if the value is null then the field is not set */		
 		getSetSelect {fieldName, value ->
 			getSetType(TYPE_SELECT, fieldName, value)
 		}
+
+		/** Retrieves the options for a select field */		
 		getSelectOptions {fieldName ->
 			getSelectField(fieldName).find(ELEMENT_OPTION)
 		}
+
+		/** Sets the value of a combo field based on the position */		
 		setSelectFromPosition {fieldName, position ->
 			def options = getSelectOptions(fieldName)
 			getSetSelect(fieldName, options[position].@value)
 		}
-		link(required: false) {id ->
-			$(ELEMENT_ANCHOR, text: id)
+
+		/** Retrieves a link based on the text for the link */
+		link(required: false) {linkText ->
+			$(ELEMENT_ANCHOR, text: linkText)
 		}
-		select {
-			id, detailsPage -> link(id).click(detailsPage)
+
+		/**	Selects a link based on the text for the link and checks that it goes tho the specified page */	
+		select {linkText, detailsPage ->
+			link(linkText).click(detailsPage)
 		}
+	
+		/** Populates the form fields given the supplied field and select maps
+		 * The field and select maps, map a field name to its value
+		 * If a select field triggers an ajax call then you will want to set pauseAfterSelectUpdate to true, to give the ajax calla chance to execute 
+		 */
 		populateFields {fieldMap, selectMap, pauseAfterSelectUpdate ->
 			fieldMap.each() {fieldName, value ->
 				if (value != null) {
@@ -75,22 +101,33 @@ class BasePage extends Page {
 			}
 			return(true)
 		}
-		createOrSelect {id, fieldMap, selectMap, createButtonName, homePage, detailsPage ->
-			if (link(id).size() == 0) {
+
+		/** If the linkText exists then it will goto that link checking that it arrives at the DetailsPage
+		 * If the linkText does not exist, it will populate the fields using the field and select maps,
+		 * once create it assumes the linkText exists and will goto that link
+		 */
+		createOrSelect {linkText, fieldMap, selectMap, createButtonName, homePage, detailsPage ->
+			if (link(linkText).size() == 0) {
 				createBase(fieldMap, selectMap, createButtonName, homePage)
-				select(id, detailsPage)
+				select(linkText, detailsPage)
 			} else {
-				select(id, detailsPage)
+				select(linkText, detailsPage)
 			}
 		}
+
+		/** Populates the form fields using the field and select maps and clicks the button to submit the form then waits for the homePage to appear */
 		createBase {fieldMap, selectMap, createButtonName, homePage ->
 			populateFields(fieldMap, selectMap, false)
 			clickButtonWait(createButtonName, homePage)
 		}
+
+		/** Populates the form fields using the field and select maps and clicks the button to submit the form */
 		updateDetailsBase {fieldMap, selectMap, updateButton, pauseAfterSelectUpdate ->
 			populateFields(fieldMap, selectMap, pauseAfterSelectUpdate)
 			clickButton(updateButton)
-		} 
+		}
+
+		/** Verifies that the values in the form match up to the values in the field and select maps */		 
 		verifyDetailsBase {fieldMap, selectMap ->
 			def result = true
 			if(fieldMap !=null){
@@ -109,6 +146,10 @@ class BasePage extends Page {
 			}
 			return(result)
 		}
+
+		/** Clicks the form button with the given name and waits for the specified page to be displayed,
+		 * If page is null, then it does not wait 
+		 */
 		clickButtonWait {buttonName, page ->
 			if (page == null) {
 				getInputField(buttonName).click()
@@ -116,6 +157,8 @@ class BasePage extends Page {
 				getInputField(buttonName).click(page)
 			}
 		}
+
+		/** Clicks the specified form button **/		
 		clickButton {buttonName ->
 			clickButtonWait(buttonName, null)
 		}
