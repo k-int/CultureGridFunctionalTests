ef1f90c3538cf8c888d9b34a65ad129907ffaccf
ChasWoodfield <chas.woodfield@k-int.co.uk>
2014-02-24 10:05:24 +0000

    It is only a small script, but off course there had to be errors in it, when it appeared to run ok, didn't realise I had a tests results window open when I ran it ....

--- RunTests.bat
+++ RunTests.bat
@@ -1,6 +1,6 @@
 echo running all functional tests for Culture Grid, it is assumed that 
 echo GRAILS_HOME is set pointing at a 2.3.5 installation and its bin directory is on the path
 
-grails test-app -echoOut functional: MenuSpec DataSpec "*"
+call grails test-app -echoOut functional: MenuSpec DataSpec "*"
 
-target/test-reports/html/all.html
+target\test-reports\html\all.html
